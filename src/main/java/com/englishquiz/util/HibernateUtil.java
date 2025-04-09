dcb4a98f70d514f60f85676500131e2cad544405
Gabriel Castro <gabrielcastro.developer@gmail.com>
2025-04-09 13:48:18 -0300

    fixed dependencies and env variables setted

--- src/main/java/com/englishquiz/util/HibernateUtil.java
+++ src/main/java/com/englishquiz/util/HibernateUtil.java
@@ -1,5 +1,6 @@
 package com.englishquiz.util;
 
+import io.github.cdimascio.dotenv.Dotenv;
 import org.hibernate.SessionFactory;
 import org.hibernate.cfg.Configuration;
 
@@ -8,11 +9,22 @@ public class HibernateUtil {
 
     static {
         try {
-            sessionFactory = new Configuration()
+            Dotenv dotenv = Dotenv.load();
+
+            Configuration configuration = new Configuration()
                 .configure("hibernate.cfg.xml")
                 .addAnnotatedClass(com.englishquiz.model.User.class)
-                .buildSessionFactory();
+                .addAnnotatedClass(com.englishquiz.model.Level.class)
+                .addAnnotatedClass(com.englishquiz.model.Question.class);
+
+            // Sobrescreve propriedades do XML com os valores do .env
+            configuration.setProperty("hibernate.connection.url", dotenv.get("DB_URL"));
+            configuration.setProperty("hibernate.connection.username", dotenv.get("DB_USER"));
+            configuration.setProperty("hibernate.connection.password", dotenv.get("DB_PASSWORD"));
+
+            sessionFactory = configuration.buildSessionFactory();
         } catch (Throwable ex) {
+            System.err.println("Erro ao inicializar o Hibernate: " + ex);
             throw new ExceptionInInitializerError(ex);
         }
     }
