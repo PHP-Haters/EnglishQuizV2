8014b13b33d89e90f0fa5817c0a38b0e69d060f7
Ana Ripoli <anaripoli.eng@gmail.com>
2025-06-02 20:28:20 -0300

    initial view on panels

--- src/main/java/com/englishquiz/Main.java
+++ src/main/java/com/englishquiz/Main.java
@@ -1,14 +1,18 @@
 package com.englishquiz;
 
-
 import com.englishquiz.controller.UserController;
 import com.englishquiz.service.AnswerService;
 import com.englishquiz.service.LevelService;
 import com.englishquiz.service.QuestionService;
 import com.englishquiz.service.UserService;
+import com.englishquiz.view.Home;
 
 public class Main {
     public static void main(String[] args) {
+
+        
+        Home frame = new Home();
+        frame.setVisible(true);
         
         UserService userService = new UserService();
         userService.criarUserPadrao();
---
+++ src/main/java/com/englishquiz/view/Home.java
@@ -0,0 +1,116 @@
+package com.englishquiz.view;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+import javax.swing.JButton;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+import javax.swing.GroupLayout;
+import javax.swing.GroupLayout.Alignment;
+import javax.swing.JTextArea;
+import javax.swing.LayoutStyle.ComponentPlacement;
+import javax.swing.JLabel;
+import java.awt.Font;
+import javax.swing.JTextPane;
+import java.awt.Color;
+
+public class Home extends JFrame {
+    
+	private static final long serialVersionUID = 1L;
+	private JPanel contentPane;
+
+	public Home() {
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 450, 300);
+		contentPane = new JPanel();
+		contentPane.setBackground(new Color(244, 105, 81));
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+
+		setContentPane(contentPane);
+		
+		JButton btnNewButton = new JButton("Login");
+		btnNewButton.setFont(new Font("Yu Gothic Medium", Font.BOLD, 14));
+		btnNewButton.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+			}
+		});
+		
+		JLabel lblNewLabel = new JLabel("Welcome to English Quiz!");
+		lblNewLabel.setBackground(new Color(0, 0, 0));
+		lblNewLabel.setForeground(new Color(0, 0, 0));
+		lblNewLabel.setFont(new Font("Yu Gothic", Font.BOLD, 24));
+		
+		JTextPane textPane = new JTextPane();
+		
+		JTextPane textPane_1 = new JTextPane();
+		
+		JLabel lblNewLabel_1 = new JLabel("Email:");
+		lblNewLabel_1.setFont(new Font("Yu Gothic UI Semibold", Font.BOLD, 16));
+		
+		JLabel lblNewLabel_2 = new JLabel("Password:");
+		lblNewLabel_2.setFont(new Font("Yu Gothic UI Semibold", Font.BOLD, 16));
+		
+		JButton btnNewButton_1 = new JButton("Register");
+		btnNewButton_1.setFont(new Font("Yu Gothic Medium", Font.BOLD, 14));
+		btnNewButton_1.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+                
+			}
+		});
+		
+		JLabel lblNewLabel_3 = new JLabel("or");
+		lblNewLabel_3.setFont(new Font("Yu Gothic Medium", Font.BOLD, 14));
+		GroupLayout gl_contentPane = new GroupLayout(contentPane);
+		gl_contentPane.setHorizontalGroup(
+			gl_contentPane.createParallelGroup(Alignment.LEADING)
+				.addGroup(gl_contentPane.createSequentialGroup()
+					.addGap(56)
+					.addComponent(lblNewLabel))
+				.addGroup(gl_contentPane.createSequentialGroup()
+					.addGap(83)
+					.addComponent(lblNewLabel_1, GroupLayout.PREFERRED_SIZE, 45, GroupLayout.PREFERRED_SIZE)
+					.addGap(72)
+					.addComponent(textPane, GroupLayout.PREFERRED_SIZE, 110, GroupLayout.PREFERRED_SIZE))
+				.addGroup(gl_contentPane.createSequentialGroup()
+					.addGap(83)
+					.addComponent(lblNewLabel_2)
+					.addGap(44)
+					.addComponent(textPane_1, GroupLayout.PREFERRED_SIZE, 110, GroupLayout.PREFERRED_SIZE))
+				.addGroup(gl_contentPane.createSequentialGroup()
+					.addGap(85)
+					.addComponent(btnNewButton)
+					.addGap(18)
+					.addComponent(lblNewLabel_3, GroupLayout.PREFERRED_SIZE, 18, GroupLayout.PREFERRED_SIZE)
+					.addGap(19)
+					.addComponent(btnNewButton_1))
+		);
+
+		gl_contentPane.setVerticalGroup(
+			gl_contentPane.createParallelGroup(Alignment.LEADING)
+				.addGroup(gl_contentPane.createSequentialGroup()
+					.addGap(21)
+					.addComponent(lblNewLabel)
+					.addGap(18)
+					.addGroup(gl_contentPane.createParallelGroup(Alignment.LEADING)
+						.addComponent(lblNewLabel_1)
+						.addGroup(gl_contentPane.createSequentialGroup()
+							.addGap(3)
+							.addComponent(textPane, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE)))
+					.addGap(10)
+					.addGroup(gl_contentPane.createParallelGroup(Alignment.LEADING)
+						.addComponent(lblNewLabel_2)
+						.addGroup(gl_contentPane.createSequentialGroup()
+							.addGap(3)
+							.addComponent(textPane_1, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE)))
+					.addGap(39)
+					.addGroup(gl_contentPane.createParallelGroup(Alignment.LEADING)
+						.addComponent(btnNewButton)
+						.addGroup(gl_contentPane.createSequentialGroup()
+							.addGap(4)
+							.addComponent(lblNewLabel_3))
+						.addComponent(btnNewButton_1)))
+		);
+		contentPane.setLayout(gl_contentPane);
+	}
+}
