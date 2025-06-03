138d063e34ebceba02565b6a36aa34bedb5d6edc
Ana Ripoli <anaripoli.eng@gmail.com>
2025-06-02 21:06:38 -0300

    page PerfilOptions swing

---
+++ src/main/java/com/englishquiz/view/PerfilOptions.java
@@ -0,0 +1,97 @@
+package com.englishquiz.view;
+
+import javax.swing.*;
+import javax.swing.border.*;
+
+import com.englishquiz.controller.UserController;
+
+import java.awt.*;
+import java.awt.event.*;
+
+import javax.swing.GroupLayout.Alignment;
+
+public class PerfilOptions {
+
+public class PerfilOption extends JFrame {
+
+	private static final long serialVersionUID = 1L;
+	private JPanel contentPane;
+
+	public PerfilOption() {
+        UserController userController = new UserController();
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 450, 300);
+		contentPane = new JPanel();
+		contentPane.setBackground(new Color(245, 106, 82));
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+
+		setContentPane(contentPane);
+		
+		JButton btnNewButton_3 = new JButton("Mostrar usuário");
+		btnNewButton_3.setFont(new Font("Yu Gothic Medium", Font.BOLD, 14));
+		btnNewButton_3.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+			}
+		});
+		
+		JButton btnNewButton_6 = new JButton("Editar email");
+		btnNewButton_6.setFont(new Font("Yu Gothic Medium", Font.BOLD, 14));
+		btnNewButton_6.setForeground(new Color(0, 0, 0));
+		
+		JButton btnNewButton_5 = new JButton("Editar senha");
+		btnNewButton_5.setFont(new Font("Yu Gothic Medium", Font.BOLD, 14));
+		
+		JButton btnNewButton_2 = new JButton("Deletar usuário");
+		btnNewButton_2.setFont(new Font("Yu Gothic Medium", Font.BOLD, 14));
+		
+		JButton btnNewButton_4 = new JButton("Deslogar");
+		btnNewButton_4.setFont(new Font("Yu Gothic Medium", Font.BOLD, 14));
+		
+		JButton btnNewButton_1 = new JButton("Voltar");
+		btnNewButton_1.setFont(new Font("Yu Gothic Medium", Font.BOLD, 14));
+		
+		JLabel lblNewLabel = new JLabel("English Quiz");
+		lblNewLabel.setFont(new Font("Yu Gothic UI Semibold", Font.BOLD, 20));
+		GroupLayout gl_contentPane = new GroupLayout(contentPane);
+		gl_contentPane.setHorizontalGroup(
+			gl_contentPane.createParallelGroup(Alignment.LEADING)
+				.addGroup(gl_contentPane.createSequentialGroup()
+					.addGap(39)
+					.addGroup(gl_contentPane.createParallelGroup(Alignment.LEADING)
+						.addComponent(lblNewLabel, GroupLayout.PREFERRED_SIZE, 116, GroupLayout.PREFERRED_SIZE)
+						.addGroup(gl_contentPane.createSequentialGroup()
+							.addComponent(btnNewButton_3)
+							.addGap(40)
+							.addComponent(btnNewButton_6, GroupLayout.PREFERRED_SIZE, 141, GroupLayout.PREFERRED_SIZE))
+						.addGroup(gl_contentPane.createSequentialGroup()
+							.addComponent(btnNewButton_5, GroupLayout.PREFERRED_SIZE, 145, GroupLayout.PREFERRED_SIZE)
+							.addGap(40)
+							.addComponent(btnNewButton_2))
+						.addGroup(gl_contentPane.createSequentialGroup()
+							.addComponent(btnNewButton_4, GroupLayout.PREFERRED_SIZE, 145, GroupLayout.PREFERRED_SIZE)
+							.addGap(40)
+							.addComponent(btnNewButton_1, GroupLayout.PREFERRED_SIZE, 141, GroupLayout.PREFERRED_SIZE)))
+					.addGap(61))
+		);
+		gl_contentPane.setVerticalGroup(
+			gl_contentPane.createParallelGroup(Alignment.LEADING)
+				.addGroup(gl_contentPane.createSequentialGroup()
+					.addGap(13)
+					.addComponent(lblNewLabel)
+					.addGap(18)
+					.addGroup(gl_contentPane.createParallelGroup(Alignment.LEADING)
+						.addComponent(btnNewButton_3)
+						.addComponent(btnNewButton_6))
+					.addGap(18)
+					.addGroup(gl_contentPane.createParallelGroup(Alignment.LEADING)
+						.addComponent(btnNewButton_5)
+						.addComponent(btnNewButton_2))
+					.addGap(18)
+					.addGroup(gl_contentPane.createParallelGroup(Alignment.LEADING)
+						.addComponent(btnNewButton_4)
+						.addComponent(btnNewButton_1)))
+		);
+		contentPane.setLayout(gl_contentPane);
+	}
+}
+}
