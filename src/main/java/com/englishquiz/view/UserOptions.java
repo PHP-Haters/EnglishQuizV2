f27296607c91d3d3cfa9fff0747e020292e17efa
Ana Ripoli <anaripoli.eng@gmail.com>
2025-06-02 21:01:09 -0300

    page UserOptions swing

---
+++ src/main/java/com/englishquiz/view/UserOptions.java
@@ -0,0 +1,90 @@
+package com.englishquiz.view;
+
+import javax.swing.*;
+import javax.swing.border.*;
+
+import com.englishquiz.controller.UserController;
+
+import java.awt.*;
+
+import javax.swing.GroupLayout.Alignment;
+
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+
+public class UserOptions {
+
+public class Options extends JFrame {
+
+	private static final long serialVersionUID = 1L;
+	private JPanel contentPane;
+
+	public Options() {
+        UserController userController = new UserController();
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 499, 306);
+		contentPane = new JPanel();
+		contentPane.setBackground(new Color(255, 255, 255));
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+
+		setContentPane(contentPane);
+		
+		JLabel lblNewLabel = new JLabel("Escolha uma opção:");
+		lblNewLabel.setFont(new Font("Yu Gothic UI Semibold", Font.BOLD, 20));
+		
+		JButton btnNewButton = new JButton("Next");
+		btnNewButton.setBackground(new Color(0, 0, 0));
+		btnNewButton.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+			}
+		});
+		btnNewButton.setFont(new Font("Yu Gothic Medium", Font.BOLD, 14));
+		
+		JRadioButton rdbtnNewRadioButton = new JRadioButton("Informações do usuário");
+		rdbtnNewRadioButton.setForeground(new Color(255, 255, 255));
+		rdbtnNewRadioButton.setBackground(new Color(250, 105, 80));
+		rdbtnNewRadioButton.setFont(new Font("Yu Gothic UI Semibold", Font.BOLD, 17));
+		
+		JRadioButton rdbtnNewRadioButton_1 = new JRadioButton("Níveis");
+		rdbtnNewRadioButton_1.setBackground(new Color(250, 105, 80));
+		rdbtnNewRadioButton_1.setForeground(new Color(255, 255, 255));
+		rdbtnNewRadioButton_1.setFont(new Font("Yu Gothic UI Semibold", Font.BOLD, 17));
+		
+		JRadioButton rdbtnNewRadioButton_2 = new JRadioButton("Sair");
+		rdbtnNewRadioButton_2.setBackground(new Color(250, 105, 80));
+		rdbtnNewRadioButton_2.setForeground(new Color(255, 255, 255));
+		rdbtnNewRadioButton_2.setFont(new Font("Yu Gothic UI Semibold", Font.BOLD, 17));
+		GroupLayout gl_contentPane = new GroupLayout(contentPane);
+		gl_contentPane.setHorizontalGroup(
+			gl_contentPane.createParallelGroup(Alignment.LEADING)
+				.addGroup(gl_contentPane.createSequentialGroup()
+					.addGap(28)
+					.addGroup(gl_contentPane.createParallelGroup(Alignment.LEADING)
+						.addComponent(rdbtnNewRadioButton_1)
+						.addComponent(rdbtnNewRadioButton, GroupLayout.PREFERRED_SIZE, 215, GroupLayout.PREFERRED_SIZE)
+						.addComponent(lblNewLabel)
+						.addGroup(gl_contentPane.createSequentialGroup()
+							.addComponent(rdbtnNewRadioButton_2, GroupLayout.PREFERRED_SIZE, 99, GroupLayout.PREFERRED_SIZE)
+							.addGap(199)
+							.addComponent(btnNewButton, GroupLayout.PREFERRED_SIZE, 125, GroupLayout.PREFERRED_SIZE)))
+					.addGap(24))
+		);
+		gl_contentPane.setVerticalGroup(
+			gl_contentPane.createParallelGroup(Alignment.LEADING)
+				.addGroup(gl_contentPane.createSequentialGroup()
+					.addGap(21)
+					.addComponent(lblNewLabel, GroupLayout.PREFERRED_SIZE, 42, GroupLayout.PREFERRED_SIZE)
+					.addGap(18)
+					.addComponent(rdbtnNewRadioButton, GroupLayout.PREFERRED_SIZE, 39, GroupLayout.PREFERRED_SIZE)
+					.addGap(18)
+					.addComponent(rdbtnNewRadioButton_1, GroupLayout.PREFERRED_SIZE, 39, GroupLayout.PREFERRED_SIZE)
+					.addGap(18)
+					.addGroup(gl_contentPane.createParallelGroup(Alignment.BASELINE)
+						.addComponent(rdbtnNewRadioButton_2)
+						.addComponent(btnNewButton, GroupLayout.PREFERRED_SIZE, 40, GroupLayout.PREFERRED_SIZE)))
+		);
+		contentPane.setLayout(gl_contentPane);
+	}
+}
+
+}
